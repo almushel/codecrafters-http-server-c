70cfac774bb0c3f6f606ce9cce0c492c03e22887
Andrew Mushel <almushel@protonmail.com>
2024-07-16 14:51:52 -0500

    codecrafters submit [skip ci]

--- app/server.c
+++ app/server.c
@@ -7,24 +7,26 @@
 #include <errno.h>
 #include <unistd.h>
 
+typedef struct http_status {
+	float version;
+	int code;
+	char* reason;
+} http_status;
+
 int main() {
 	// Disable output buffering
 	setbuf(stdout, NULL);
 	setbuf(stderr, NULL);
 
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	printf("Logs from your program will appear here!\n");
+	int server_fd;
+	socklen_t client_addr_len;
+	struct sockaddr_in client_addr;
 
-	// Uncomment this block to pass the first stage
-
-	 int server_fd, client_addr_len;
-	 struct sockaddr_in client_addr;
-
-	 server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	 if (server_fd == -1) {
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd == -1) {
 		printf("Socket creation failed: %s...\n", strerror(errno));
 		return 1;
-	 }
+	}
 
 	// Since the tester restarts your program quite often, setting SO_REUSEADDR
 	// ensures that we don't run into 'Address already in use' errors
@@ -34,29 +36,42 @@ int main() {
 		return 1;
 	 }
 
-	 struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
-									 .sin_port = htons(4221),
-									 .sin_addr = { htonl(INADDR_ANY) },
-									};
+	struct sockaddr_in serv_addr = {
+		.sin_family = AF_INET ,
+		.sin_port = htons(4221),
+		.sin_addr = { htonl(INADDR_ANY) },
+	};
 
-	 if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
+	if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
 		printf("Bind failed: %s \n", strerror(errno));
 		return 1;
-	 }
+	}
 
-	 int connection_backlog = 5;
-	 if (listen(server_fd, connection_backlog) != 0) {
+	int connection_backlog = 5;
+	if (listen(server_fd, connection_backlog) != 0) {
 		printf("Listen failed: %s \n", strerror(errno));
 		return 1;
-	 }
+	}
 
-	 printf("Waiting for a client to connect...\n");
-	 client_addr_len = sizeof(client_addr);
+	printf("Waiting for a client to connect...\n");
+	client_addr_len = sizeof(client_addr);
 
-	 accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
-	 printf("Client connected\n");
+	int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+	printf("Client connected\n");
 
-	 close(server_fd);
+	http_status status = {
+		.version = 1.1,
+		.code = 200,
+		.reason = "OK"
+	};
+	char response[256];
+	sprintf(response, "HTTP/%.1f %u %s\r\n\r\n", status.version, status.code, status.reason);
 
-return 0;
+	send(client_fd, response, strlen(response), 0);
+
+	printf("Status response sent");
+
+	close(server_fd);
+
+	return 0;
 }
