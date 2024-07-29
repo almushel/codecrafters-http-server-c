e90ed243a0c54327acdefd8a49d4b3ee04406612
Andrew Mushel <almushel@protonmail.com>
2024-07-29 13:51:38 -0500

    codecrafters submit [skip ci]

--- app/server.c
+++ app/server.c
@@ -7,6 +7,7 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <pthread.h>
 
 typedef struct key_val {
 	char* key;
@@ -41,69 +42,27 @@ typedef struct http_request {
 } http_request;
 
 
-int main() {
-	// Disable output buffering
-	setbuf(stdout, NULL);
-	setbuf(stderr, NULL);
-
-	int server_fd;
-	socklen_t client_addr_len;
-	struct sockaddr_in client_addr;
-
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd == -1) {
-		printf("Socket creation failed: %s...\n", strerror(errno));
-		return 1;
-	}
-
-	// Since the tester restarts your program quite often, setting SO_REUSEADDR
-	// ensures that we don't run into 'Address already in use' errors
-	 int reuse = 1;
-	 if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-		printf("SO_REUSEADDR failed: %s \n", strerror(errno));
-		return 1;
-	 }
-
-	struct sockaddr_in serv_addr = {
-		.sin_family = AF_INET ,
-		.sin_port = htons(4221),
-		.sin_addr = { htonl(INADDR_ANY) },
-	};
-
-	if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
-		printf("Bind failed: %s \n", strerror(errno));
-		return 1;
-	}
-
-	int connection_backlog = 5;
-	if (listen(server_fd, connection_backlog) != 0) {
-		printf("Listen failed: %s \n", strerror(errno));
-		return 1;
-	}
-
-	printf("Waiting for a client to connect...\n");
-
-	client_addr_len = sizeof(client_addr);
-	int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
-	printf("Client connected\n");
+void* process_request(void* cfd) {
+	int client_fd = (int)(long)cfd;
 
 	char request_buf[256];
 	int bytes_read;
 	if ( (bytes_read = recv(client_fd, request_buf, 256, 0)) < 0) {
 		printf("Recv failed: %s \n", strerror(bytes_read));
-		return 1;
+		return (void*)1;
 	}
 
 	int lines_size = 8;
-	char** lines = malloc(sizeof(char*) * lines_size);
-	lines[0] = request_buf;
 	int line_count = 1;
+	char* lines[lines_size];
+	lines[0] = request_buf;
 	for (char* c = request_buf; c != (request_buf+bytes_read); c++) {
 		if (c[0] == '\r' && c[1] == '\n') {
 			if (line_count == lines_size) {
-				lines_size *=2;
-				lines = realloc(lines, sizeof(char*) * lines_size);
+				printf("Max request lines reached: %i\n", lines_size);
+				break;
 			}
+
 			*c = '\0';
 			lines[line_count] = (c+2);
 			line_count++;
@@ -114,24 +73,24 @@ int main() {
 	printf("Parsing request header...\n");
 	if ( sscanf(lines[0], "%s %s HTTP/%u.%u\r\n", request.method, request.target, &request.version_major, &request.version_minor) != 4) {
 		printf("Invalid request header\n");
-		return 1;
+		return (void*)1;
 	}
 
 	if (strcmp(request.method, "GET") != 0) {
 		printf("Unsupported request method. Supported: GET\n");
-		return 1;
+		return (void*)1;
 	}
 
 	if (request.version_major != 1 || request.version_minor != 1) {
 		// Status code 505
 		printf("Unsupported HTTP version %u.%u. Requires 1.1\n", request.version_major, request.version_minor);
-		return 1;
+		return (void*)1;
 	}
 
 	// NOTE: lines[line_count-1] should be the request body
-	
 	request.headers_size = 8;
-	request.headers = malloc(sizeof(key_val)*request.headers_size);
+	key_val request_headers[8];
+	request.headers = request_headers;
 	for (int i = 1; i < line_count-1; i++) {
 		char* left = lines[i]; 
 		char* right = 0;	
@@ -150,8 +109,8 @@ int main() {
 		
 		if (left && right) {
 			if (request.num_headers == request.headers_size) {
-				request.headers_size *= 2;
-				request.headers = realloc(request.headers, request.headers_size);
+				printf("Max request headers reached: %i", request.headers_size);
+				break;
 			}
 			request.headers[request.num_headers-1] = (key_val) {
 				.key = left,
@@ -193,10 +152,13 @@ int main() {
 		response.status.reason = "Not Found";
 	}
 
+	response.headers_size = 2;
+	char* response_headers[response.headers_size];
 	if (response.body) {
-		response.num_headers = response.headers_size = 2;
-		response.headers = malloc(sizeof(char*) * response.headers_size);
+		response.num_headers = response.headers_size;
+		response.headers = response_headers;
 		response.headers[0] = "Content-Type: text/plain\r\n";
+
 		char content_length[128];
 		sprintf(content_length, "Content-Length: %lu\r\n", strlen(response.body));
 		response.headers[1] = content_length;
@@ -231,6 +193,66 @@ int main() {
 
 	printf("Sending response...\n");
 	send(client_fd, response_str, strlen(response_str), 0);
+	close(client_fd);
+
+	return (void*)0;
+}
+
+int main() {
+	// Disable output buffering
+	setbuf(stdout, NULL);
+	setbuf(stderr, NULL);
+
+	int server_fd;
+	socklen_t client_addr_len;
+	struct sockaddr_in client_addr;
+
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd == -1) {
+		printf("Socket creation failed: %s...\n", strerror(errno));
+		return 1;
+	}
+
+	// Since the tester restarts your program quite often, setting SO_REUSEADDR
+	// ensures that we don't run into 'Address already in use' errors
+	 int reuse = 1;
+	 if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+		printf("SO_REUSEADDR failed: %s \n", strerror(errno));
+		return 1;
+	 }
+
+	struct sockaddr_in serv_addr = {
+		.sin_family = AF_INET ,
+		.sin_port = htons(4221),
+		.sin_addr = { htonl(INADDR_ANY) },
+	};
+
+	if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
+		printf("Bind failed: %s \n", strerror(errno));
+		return 1;
+	}
+
+	int connection_backlog = 5;
+	int max_threads = 8;
+	int num_threads = 0;
+	pthread_t threads[max_threads];
+
+	while(1) {
+		printf("Waiting for a client to connect...\n");
+		if (listen(server_fd, connection_backlog) != 0) {
+			printf("Listen failed: %s \n", strerror(errno));
+			return 1;
+		}
+
+		client_addr_len = sizeof(client_addr);
+		int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+		if (client_fd == -1) continue;
+
+		printf("Client connected\n");
+		
+		pthread_t thread;
+		pthread_create(&thread, 0, process_request, (void*)(long)client_fd);
+	}
 
 	close(server_fd);
 
